448ac76b2b7f48f16fdf9840b90fe797bbd239d9
Erhan Karakaya <erhan277@gmail.com>
2017-12-18 15:31:30 +0300

    Added NoRepositoryBean annotation

--- merepository/src/main/java/com/teammental/merepository/BaseJpaRepository.java
+++ merepository/src/main/java/com/teammental/merepository/BaseJpaRepository.java
@@ -5,7 +5,9 @@ import com.teammental.meentity.BaseEntity;
 import java.io.Serializable;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
 
+@NoRepositoryBean
 public interface BaseJpaRepository<EntityT extends BaseEntity, IdT extends Serializable>
     extends JpaRepository<EntityT, IdT> {
 }
