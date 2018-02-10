d4e6768415072f3d056a307dd303e7d18462f927
Erhan KARAKAYA <erhan277@gmail.com>
2018-02-10 21:40:27 +0300

    Added example getter

--- medto/src/main/java/com/teammental/medto/FilterDto.java
+++ medto/src/main/java/com/teammental/medto/FilterDto.java
@@ -1,9 +1,12 @@
 package com.teammental.medto;
 
 import com.teammental.mecore.stereotype.dto.Dto;
+import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Pageable;
 
-public interface FilterDto extends Dto {
+public interface FilterDto<DtoT extends Dto> extends Dto {
 
   Pageable getPage();
+
+  Example<DtoT> getExample();
 }
