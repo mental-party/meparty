1fb42d8f76a4ab7ec03395b2f1624e5060173bfe
Erhan Karakaya <erhan277@gmail.com>
2018-03-05 21:01:00 +0300

    Added isPrimitiveOrWrapper method

--- mecore/src/main/java/com/teammental/mehelper/PrimitiveHelper.java
+++ mecore/src/main/java/com/teammental/mehelper/PrimitiveHelper.java
@@ -95,8 +95,8 @@ public class PrimitiveHelper {
 
   /**
    * Checks if the given clazz is wrapper or primitive.
-   * @param clazz
-   * @return
+   * @param clazz type
+   * @return true if clazz is wrapper or primitive
    */
   public static boolean isWrapperOrPrimitive(Class<?> clazz) {
     return wrapperMap.containsKey(clazz)
