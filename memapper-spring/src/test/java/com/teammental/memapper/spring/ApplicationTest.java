54456d1166697a9d5e3cef1ca008e05dda1cfbf7
Erhan KARAKAYA <erhan277@gmail.com>
2018-09-22 15:09:53 +0300

    Added test for default mapper bean config

--- memapper-spring/src/test/java/com/teammental/memapper/spring/ApplicationTest.java
+++ memapper-spring/src/test/java/com/teammental/memapper/spring/ApplicationTest.java
@@ -1,20 +1,34 @@
 package com.teammental.memapper.spring;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
-import com.teammental.memapper.MeMapper;
+import com.teammental.memapper.Mapper;
 import com.teammental.memapper.spring.to.TargetTo;
 import com.teammental.memapper.spring.to.SourceTo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @Import({StartupMapConfigurationRegistry.class,
     StartupMapConfigurationRegistryBeanLoader.class,
+    StartupDefaultMapperBeanConfiguration.class,
     TestConfig.class})
 public class ApplicationTest {
+
+  @Autowired
+  private Mapper mapper;
+
+
+  @Test
+  public void mapperBeanShouldNotBeNull() {
+
+    assertNotNull(mapper);
+  }
+
 
   @Test
   public void shouldMapAsConfigured() {
@@ -23,7 +37,7 @@ public class ApplicationTest {
     source.setId(894325);
     source.setName("43w5t43543");
 
-    TargetTo target = (TargetTo) MeMapper.from(source).to(TargetTo.class);
+    TargetTo target = mapper.map(source, TargetTo.class);
 
     assertEquals(source.getId(), target.getNo());
     assertEquals(source.getName(), target.getTitle());
