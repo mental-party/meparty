6c821f0f9c66f176e20e78744e608a2692c164e9
Erhan Karakaya <erhan277@gmail.com>
2018-10-10 19:47:22 +0300

    Added extensionId

--- mecore/src/main/java/com/teammental/mecore/enums/FileExtension.java
+++ mecore/src/main/java/com/teammental/mecore/enums/FileExtension.java
@@ -4,29 +4,31 @@ import java.util.Arrays;
 import java.util.Locale;
 
 public enum FileExtension {
-  JPG(FileType.IMAGE, "jpg", "jpeg"),
-  PNG(FileType.IMAGE, "png"),
-  GIF(FileType.IMAGE, "gif"),
-  BMP(FileType.IMAGE, "bmp"),
-  MP4(FileType.VIDEO, "mp4"),
-  MP3(FileType.AUDIO, "mp3"),
-  XLS(FileType.DOCUMENT, "xls"),
-  XLSX(FileType.DOCUMENT, "xlsx"),
-  DOC(FileType.DOCUMENT, "doc"),
-  DOCX(FileType.DOCUMENT, "docx"),
-  TXT(FileType.TEXT, "txt"),
-  PDF(FileType.DOCUMENT, "pdf"),
-  RTF(FileType.DOCUMENT, "rtf"),
-  ZIP(FileType.COMPRESSED, "zip"),
-  RAR(FileType.COMPRESSED, "rar");
+  JPG(FileType.IMAGE, 1, "jpg", "jpeg"),
+  PNG(FileType.IMAGE, 2, "png"),
+  GIF(FileType.IMAGE, 3, "gif"),
+  BMP(FileType.IMAGE, 4, "bmp"),
+  MP4(FileType.VIDEO, 5, "mp4"),
+  MP3(FileType.AUDIO, 6, "mp3"),
+  XLS(FileType.DOCUMENT, 7, "xls"),
+  XLSX(FileType.DOCUMENT, 8, "xlsx"),
+  DOC(FileType.DOCUMENT, 8, "doc"),
+  DOCX(FileType.DOCUMENT, 10, "docx"),
+  TXT(FileType.TEXT, 11, "txt"),
+  PDF(FileType.DOCUMENT, 12, "pdf"),
+  RTF(FileType.DOCUMENT, 13, "rtf"),
+  ZIP(FileType.COMPRESSED, 14, "zip"),
+  RAR(FileType.COMPRESSED, 15, "rar");
 
   private String[] extensions;
   private FileType fileType;
+  private int extensionId;
 
-  FileExtension(FileType fileType, String... extensions) {
+  FileExtension(FileType fileType, int extensionId, String... extensions) {
 
     this.fileType = fileType;
     this.extensions = extensions;
+    this.extensionId = extensionId;
   }
 
   public String[] getExtensions() {
@@ -39,8 +41,13 @@ public enum FileExtension {
     return fileType;
   }
 
+  public int getExtensionId() {
+    return extensionId;
+  }
+
   /**
    * Checks if given extension is matching.
+   *
    * @param extension extension
    * @return true if matches
    */
