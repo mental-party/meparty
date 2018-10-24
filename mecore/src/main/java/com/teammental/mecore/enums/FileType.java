b663c6705341721688d73bef4e43b01ca8a58771
Erhan Karakaya <erhan277@gmail.com>
2018-10-24 14:20:07 +0300

    Added new file types and extensions

--- mecore/src/main/java/com/teammental/mecore/enums/FileType.java
+++ mecore/src/main/java/com/teammental/mecore/enums/FileType.java
@@ -6,16 +6,28 @@ import java.util.stream.Collectors;
 
 public enum FileType {
 
-  IMAGE, VIDEO, AUDIO, DOCUMENT, COMPRESSED, TEXT, UNKNOWN;
+  IMAGE,
+  VIDEO,
+  AUDIO,
+  DOCUMENT,
+  COMPRESSED,
+  TEXT,
+  UNKNOWN,
+  SPREADSHEET,
+  DATA,
+  LOG,
+  PRESENTATION;
+
 
   /**
    * Gets extensions of this FileType.
+   *
    * @return EnumSet of FileExtensions
    */
   public EnumSet<FileExtension> getFileExtensions() {
     return EnumSet.copyOf(Arrays.stream(FileExtension.values())
-    .filter(fileExtension -> fileExtension.getFileType().equals(this))
-    .collect(Collectors.toSet()));
+        .filter(fileExtension -> fileExtension.getFileType().equals(this))
+        .collect(Collectors.toSet()));
   }
 
 }
