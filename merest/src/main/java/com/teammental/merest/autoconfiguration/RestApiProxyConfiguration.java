f35279154d0e86a58c7d1a3d0d67ac961c7f67da
Erhan KARAKAYA <erhan277@gmail.com>
2018-11-20 00:41:00 +0300

    Registering configuration properties on restApiApplicationRegistry bean creating

--- merest/src/main/java/com/teammental/merest/autoconfiguration/RestApiProxyConfiguration.java
+++ merest/src/main/java/com/teammental/merest/autoconfiguration/RestApiProxyConfiguration.java
@@ -2,6 +2,8 @@ package com.teammental.merest.autoconfiguration;
 
 import com.teammental.merest.RestApiProxyBeanFactory;
 import com.teammental.merest.RestApiProxyHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -13,38 +15,77 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class RestApiProxyConfiguration {
 
+  private static final Logger LOGGER
+      = LoggerFactory.getLogger(RestApiProxyConfiguration.class);
+
   private final RestTemplateBuilder restTemplateBuilder;
 
+  private RestApiApplicationConfigurationProperties restApiApplicationConfigurationProperties;
+
+  /**
+   * Constructor.
+   *
+   * @param restTemplateBuilder                       restTemplateBuilder
+   * @param restApiApplicationConfigurationProperties restApiApplicationConfigurationProperties
+   */
   @Autowired
-  public RestApiProxyConfiguration(RestTemplateBuilder restTemplateBuilder) {
+  public RestApiProxyConfiguration(RestTemplateBuilder restTemplateBuilder,
+                                   RestApiApplicationConfigurationProperties
+                                       restApiApplicationConfigurationProperties) {
+
     this.restTemplateBuilder = restTemplateBuilder;
+    this.restApiApplicationConfigurationProperties
+        = restApiApplicationConfigurationProperties;
   }
 
   @Bean
   public ApplicationExplorer applicationExplorer() {
+
     return new ApplicationExplorer();
   }
 
+  /**
+   * RestApiApplicationRegistry bean.
+   *
+   * @return RestApiApplicationRegistry
+   */
   @Bean
   public RestApiApplicationRegistry restApiApplicationRegistry() {
-    return new RestApiApplicationRegistry();
+
+    RestApiApplicationRegistry restApiApplicationRegistry
+        = new RestApiApplicationRegistry();
+
+    if (restApiApplicationConfigurationProperties == null
+        || restApiApplicationConfigurationProperties
+        .getRestApiApplications() == null) {
+      LOGGER.info("Couldn't register rest api applications from resources");
+    } else {
+
+      restApiApplicationRegistry
+          .registerRestApiApplications(restApiApplicationConfigurationProperties
+              .getRestApiApplications());
+    }
+    return restApiApplicationRegistry;
   }
 
   @Bean
   @ConditionalOnMissingBean(RestTemplate.class)
   public RestTemplate restTemplate() {
+
     return restTemplateBuilder.build();
   }
 
   @Bean
   @DependsOn("restApiApplicationRegistry")
   public RestApiProxyHandler restApiProxy() {
+
     return new RestApiProxyHandler(restTemplate());
   }
 
   @Bean(name = "restApiProxyBeanFactory")
   @DependsOn("restApiApplicationRegistry")
   public RestApiProxyBeanFactory restApiProxyBeanFactory() {
+
     return new RestApiProxyBeanFactory(restApiProxy());
   }
 }
