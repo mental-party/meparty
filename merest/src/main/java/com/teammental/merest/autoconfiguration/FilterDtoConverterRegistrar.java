705d16aa15c4a9c9d7cd6c779438a9df090b0144
Erhan Karakaya <erhan277@gmail.com>
2018-11-20 15:07:57 +0300

    Removed @EnableWebMvc annotation which is not needed in spring-boot

--- merest/src/main/java/com/teammental/merest/autoconfiguration/FilterDtoConverterRegistrar.java
+++ merest/src/main/java/com/teammental/merest/autoconfiguration/FilterDtoConverterRegistrar.java
@@ -1,14 +1,14 @@
 package com.teammental.merest.autoconfiguration;
 
 import java.util.List;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
-public class FilterDtoConverterRegistrar extends WebMvcConfigurerAdapter {
+@AutoConfigureOrder()
+public class FilterDtoConverterRegistrar implements WebMvcConfigurer {
 
 
   @Override
@@ -16,7 +16,7 @@ public class FilterDtoConverterRegistrar extends WebMvcConfigurerAdapter {
     Object[] tempArray = converters.toArray();
     converters.clear();
     converters.add(new FilterDtoConverter());
-    for (Object converter:
+    for (Object converter :
         tempArray) {
       converters.add((HttpMessageConverter<?>) converter);
     }
